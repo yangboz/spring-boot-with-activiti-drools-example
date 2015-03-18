f741dda92281fdbdd12d566db51d6de6495ae081
yangboz <YoungWelle@gmail.com>
2015-03-18 16:52:30 +0800

    CHANGED:update the file location;

--- src/test/java/org/activiti/designer/test/ProcessTestMyProcess.java
+++ src/test/java/org/activiti/designer/test/ProcessTestMyProcess.java
@@ -1,13 +1,10 @@
 package org.activiti.designer.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.io.FileInputStream;
 
-import org.activiti.engine.ProcessEngine;
-import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.runtime.ProcessInstance;
@@ -15,36 +12,34 @@ import org.activiti.engine.test.ActivitiRule;
 import org.junit.Rule;
 import org.junit.Test;
 
-public class ProcessTestMyProcess {
+public class ProcessTestMyProcess
+{
 
-	private String filename = "/Users/yangboz/Documents/Git/flaming-wight/src/main/resources/diagrams/BusinessRuleLoanProcess.bpmn";
+    private String filename =
+        "/Volumes/Transcend/git/flaming-wight/src/main/resources/diagrams/BusinessRuleLoanProcess.bpmn";
 
-	@Rule
-	public ActivitiRule activitiRule = new ActivitiRule();
+    @Rule
+    public ActivitiRule activitiRule = new ActivitiRule();
 
-	@Test
-	public void startProcess() throws Exception {
-		RepositoryService repositoryService = activitiRule
-				.getRepositoryService();
-		// TODO:Assembel the process deployment with configuration.
-		// @see:
-		repositoryService.createDeployment()
-				.addClasspathResource("diagrams/BusinessRuleLoanProcess.bpmn")
-				.addClasspathResource("diagrams/BusinessRuleLoanProcess.png")
-				.addClasspathResource("diagrams/LoanRequestRules.drl")
-				.enableDuplicateFiltering()
-				.name("businessRuleLoanProcessSimple").deploy();
-		// repositoryService.createDeployment().addInputStream("BusinessRuleLoanProcess.bpmn",
-		// new FileInputStream(filename)).deploy();
-		RuntimeService runtimeService = activitiRule.getRuntimeService();
-		Map<String, Object> variableMap = new HashMap<String, Object>();
-		variableMap.put("name", "Nadim");
-		variableMap.put("amount", 2400L);
-		variableMap.put("salary", 10000L);
-		ProcessInstance processInstance = runtimeService
-				.startProcessInstanceByKey("myProcess", variableMap);
-		assertNotNull(processInstance.getId());
-		System.out.println("id " + processInstance.getId() + " "
-				+ processInstance.getProcessDefinitionId());
-	}
+    @Test
+    public void startProcess() throws Exception
+    {
+        RepositoryService repositoryService = activitiRule.getRepositoryService();
+        // TODO:Assembel the process deployment with configuration.
+        // @see:
+        repositoryService.createDeployment().addClasspathResource("diagrams/BusinessRuleLoanProcess.bpmn")
+            .addClasspathResource("diagrams/BusinessRuleLoanProcess.png")
+            .addClasspathResource("diagrams/LoanRequestRules.drl").enableDuplicateFiltering()
+            .name("businessRuleLoanProcessSimple").deploy();
+        // repositoryService.createDeployment().addInputStream("BusinessRuleLoanProcess.bpmn",
+        // new FileInputStream(filename)).deploy();
+        RuntimeService runtimeService = activitiRule.getRuntimeService();
+        Map<String, Object> variableMap = new HashMap<String, Object>();
+        variableMap.put("name", "Nadim");
+        variableMap.put("amount", 2400L);
+        variableMap.put("salary", 10000L);
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("myProcess", variableMap);
+        assertNotNull(processInstance.getId());
+        System.out.println("id " + processInstance.getId() + " " + processInstance.getProcessDefinitionId());
+    }
 }
